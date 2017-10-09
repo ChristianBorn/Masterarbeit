0b91f973b2f5b980c7cf8b61ff2c880c8ae87a74
ChristianBorn <c.born@hhu.de>
2017-10-09 19:10:10 +0200

    Sicherung

--- link_extractor.py
+++ link_extractor.py
@@ -9,16 +9,30 @@ import requests
 import json
 import codecs
 import pandas
+#from requests.exceptions import HTTPError
+from urllib.error import HTTPError
+from socket import error as SocketError
+from http.cookiejar import CookieJar
+from time import sleep
 
 def check_social(string):
     if string == None:
         return False
-    if 'share' in string:
+    if 'share' in string or 'github' in string or 'bootstrap' in string or 'tweet' in string or 'maps' in string or 'play' in string:
         return False
-    elif 'facebook' in string or 'twitter' in string or 'youtube' in string or 'instagram' in string:
-        return True
-    else:
-        return False
+    social_channels_list = ['twitter', 'facebook', 'youtube', 'google', 
+                            'flickr', 'plus.google', 'instagram',
+                            'linkedin', 'xing', 'pinterest',
+                            'vimeo', 'foursquare', 'tumblr']
+    social = False
+    for elem in social_channels_list:
+        if elem in string:
+            social = True
+    return social
+    # elif 'facebook' in string or 'twitter' in string or 'youtube' in string or 'instagram' in string:
+    #     return True
+    # else:
+    #     return False
 
 def read_file():
     with open('Daten/170726kommunaladressen..csv', 'r') as file:
@@ -28,36 +42,128 @@ def read_file():
     #     print(row.loc['Kommune/Kreis'])
     # #print(read_input.iloc[0][:])
 
-def get_links():
+def get_links_test():
     #ToDo: Iteration über Dataframe
-    data = read_file()
+    data = pandas.DataFrame({'Kommune/Kreis':'Gütersloh','Internet':'www.guetersloh.de/'}, index=[0])
     Kommunen = {}
     social_link_filter = SoupStrainer('a', {'href':check_social})
-    linklist = ['https://www.duesseldorf.de/']
-    for link in linklist:
+    for index, row in data.iterrows():
+        Kommune = row.loc['Kommune/Kreis']
+        link = 'http://'+row.loc['Internet']
+        print('[+] Getting Links for '+Kommune)
         links_dict = {}
         all_links = []
         all_channels = []
-        website = urllib.request.urlopen(link)
+        check_again = {}
+        print('[+] Opening URL '+link)
+        req = urllib.request.Request(link)
+        opener = urllib.request.build_opener()
+        #website = urllib.request.urlopen(req)
+        website = opener.open(req)
+        print(BeautifulSoup(website).a)
+        social_links = BeautifulSoup(website, 'html.parser',parse_only=social_link_filter)
+        for social_link in social_links:
+            link_href = social_link.get('href')
+            parsed_uri = urlparse(link_href)
+            try:
+                domain = parsed_uri.hostname.split('.')[-2]
+                links_dict[domain] = link_href
+                all_links.append(link_href)
+                all_channels.append(domain)
+            except AttributeError:
+                complete_link = urlparse(link).hostname+link_href
+                all_links.append(complete_link)
+                social_channels_list = ['twitter', 'facebook', 'youtube', 'google', 
+                            'flickr', 'plus.google', 'instagram',
+                            'linkedin', 'xing', 'pinterest',
+                            'vimeo', 'foursquare', 'tumblr']
+                for elem in social_channels_list:
+                    if elem in link_href:
+                        links_dict[elem] = complete_link
+                        all_channels.append(elem)
+        print(' [+] Number of Links: '+str(len(all_links)))
+        print(' [+] Number of Channels: '+str(len(all_channels)))
+        Kommunen[Kommune] = links_dict
+        Kommunen[Kommune]['all_links'] = all_links
+        Kommunen[Kommune]['all_channels'] = list(set(all_channels))
+    print('[+] Dumping to JSON')
+    with open('Social_Links_test.json', 'w') as file:
+        json.dump(Kommunen, file, indent=2)
+    print('[+] Done')
+def get_links():
+    #ToDo: Iteration über Dataframe
+    data = read_file()
+    #data = pandas.DataFrame({'Kommune/Kreis':'Moers','Internet':'www.moers.de'}, index=[0])
+    Kommunen = {}
+    social_link_filter = SoupStrainer('a', {'href':check_social})
+    for index, row in data.iterrows():
+        Kommune = row.loc['Kommune/Kreis']
+        link = 'https://'+row.loc['Internet'].strip(' ')
+        link_https = 'http://'+row.loc['Internet'].strip(' ')
+        print('[+] Getting Links for '+Kommune)
+        links_dict = {}
+        all_links = []
+        all_channels = []
+        print('[+] Opening URL '+link)
+        try:    
+            website = urllib.request.Request(link,headers={'User-Agent' : "Magic Browser"})
+            cj = CookieJar()
+            opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+            website = opener.open(website)
+        except:
+            print('[-] Got invalid link')
+            try:
+                print('[+] Opening URL '+link_https)
+                website = urllib.request.Request(link_https,headers={'User-Agent' : "Magic Browser"})
+                cj = CookieJar()
+                opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+                website = opener.open(website)
+            except:
+                print('[-] Got invalid link')
+                with open('errors.txt', 'a') as errorlog:
+                    errorlog.write(Kommune+': '+link)
+                continue
         social_links = BeautifulSoup(website, 'html.parser', parse_only=social_link_filter)
-        for link in social_links:
-            parsed_uri = urlparse(link.get('href'))
-            domain = parsed_uri.hostname
-            domain = domain.split('.')[-2]
-            links_dict[domain] = link.get('href')
-            all_links.append(link.get('href'))
-            all_channels.append(domain)
-        Kommunen['Düsseldorf'] = links_dict
-        Kommunen['Düsseldorf']['all_links'] = all_links
-        Kommunen['Düsseldorf']['all_channels'] = list(set(all_channels))
-    print(Kommunen)
+        for social_link in social_links:
+            link_href = social_link.get('href')
+            parsed_uri = urlparse(link_href)
+            try:
+                domain = parsed_uri.hostname.split('.')[-2]
+                links_dict[domain] = link_href
+                all_links.append(link_href)
+                all_channels.append(domain)
+            # Falls die Kommune nur Redirects auf Ihre Kanäle hat
+            except AttributeError:
+                complete_link = urlparse(link).hostname+link_href
+                all_links.append(complete_link)
+                social_channels_list = ['twitter', 'facebook', 'youtube', 'google', 
+                            'flickr', 'plus.google', 'instagram',
+                            'linkedin', 'xing', 'pinterest',
+                            'vimeo', 'foursquare', 'tumblr']
+                for elem in social_channels_list:
+                    if elem in link_href:
+                        links_dict[elem] = complete_link
+                        all_channels.append(elem)
+        print(' [+] Number of Links: '+str(len(all_links)))
+        print(' [+] Number of Channels: '+str(len(all_channels)))
+        Kommunen[Kommune] = links_dict
+        Kommunen[Kommune]['all_links'] = all_links
+        Kommunen[Kommune]['all_channels'] = list(set(all_channels))
+    print('[+] Dumping to JSON')
+    with open('Social_Links.json', 'w') as file:
+        json.dump(Kommunen, file, indent=2)
+    print(['Done'])
+
         #link = link.get('href')
         #print(check_social(link))
     # print(links)
     # print(len(links))
 
+def scrape_links(data,results)
+
 def main():
-    get_links()
+    #get_links()
+    get_links_test()
     #read_file()
 
 if __name__ == '__main__':
---
+++ test_data.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import urllib.request
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+from urllib.parse import urlparse
+import requests
+import json
+import codecs
+import pandas
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+#from requests.exceptions import HTTPError
+from urllib.error import HTTPError
+from socket import error as SocketError
+from http.cookiejar import CookieJar
+
+
+def test_data():
+    empty_entries = {}
+    with open('Social_Links.json', 'r') as file:
+        collection = json.load(file)
+    for key in collection:
+        if not collection[key]['all_channels']:
+            empty_entries[key] = collection[key]
+    with open('empty_entries.json', 'w') as file:
+        json.dump(empty_entries, file, indent=2)
+
+def test_redirect():
+    link = 'http://www.guetersloh.de'
+    driver = webdriver.Chrome()
+    driver.get(link)
+    print(driver.current_url)
+    driver.close()
+    # req = urllib.request.Request(link)
+    # opener = urllib.request.build_opener()
+    # #website = urllib.request.urlopen(req)
+    # website = opener.open(req)
+    # r = requests.get(link, allow_redirects=True)
+    # print(r.history)
+    # print(website.info())
+    # print(website)
+
+
+
+def main():
+    #test_data()    
+    test_redirect()
+if __name__ == '__main__':
+    main()
